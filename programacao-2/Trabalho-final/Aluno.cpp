19433bfe8fe790a039cd656d1fff28693eb14b96
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-06 17:33:34 -0300

    refactor(Trabalho-final): faculdade\programacao-2\Trabalho-final\Aluno.cpp

---
+++ programacao-2/Trabalho-final/Aluno.cpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Pessoa.cpp"
+
+class Aluno : public Pessoa { 
+    private:
+        int matricula;
+
+    public:
+        Aluno() : Pessoa(" ") {
+            matricula = 0;
+        }
+
+        Aluno(string nome, int matricula) : Pessoa(nome) {
+            this-> matricula = matricula;
+        }
+
+        void setMatricula(int novaMatricula) {
+            matricula = novaMatricula;
+        }
+
+        int getMatricula() {
+            return matricula;
+        }
+};
