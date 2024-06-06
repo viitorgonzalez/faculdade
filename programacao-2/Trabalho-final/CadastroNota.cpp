61af0ca53c8d08fad94fec103c97bc11a9e3a756
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-06 18:49:21 -0300

    refactor(Trabalho-final): programacao-2\Trabalho-final\CadastroNota.cpp

---
+++ programacao-2/Trabalho-final/CadastroNota.cpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Nota.cpp"
+
+class CadastroNota {
+private:
+    Nota *turmaNotas;
+
+public:
+    CadastroNota() {
+        turmaNotas = new Nota[10];
+    }
+
+    Nota *getTurmaNotas() {
+        return turmaNotas;
+    }
+
+    void setTurmaNotas(Nota *turmaNotas) {
+        this->turmaNotas = turmaNotas;
+    }
+
+};
