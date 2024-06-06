a6f558c7d78ea69e6147a2ecbbe868e77971cd4c
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-06 18:49:45 -0300

    refactor(Trabalho-final): programacao-2\Trabalho-final\main.cpp

--- programacao-2/Trabalho-final/main.cpp
+++ programacao-2/Trabalho-final/main.cpp
@@ -1,8 +1,22 @@
 #include "Nota.cpp" // ok
-#include "Falta.cpp" // testing
+#include "Falta.cpp" // ok
+#include "CadastroNota.cpp" // ok
+#include "CadastroFalta.cpp" // 
 
-int main () {
+int main() {
     Aluno a;
+    Disciplina d;
+
+    CadastroNota cn;
+    CadastroFalta cf;
+
+    // CadastroNota     OK
+    cn.getTurmaNotas()[0].setN1(10);
+    cout << cn.getTurmaNotas()[0].getN1() << endl;
+
+    // CadastroFalta        testing
+    cf.getTurmaFaltas()[0].getData() = 12;
+    cout << cf.getTurmaFaltas()[0].getData();
 
     return 0;
 }
