ad8ccdbf1c31dc9a3158cb486be48ce8eed769fb
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-09 17:05:18 -0300

    programacao-2\Trabalho-final\Utils.cpp

--- programacao-2/Trabalho-final/Utils.cpp
+++ programacao-2/Trabalho-final/Utils.cpp
@@ -26,7 +26,7 @@ class Utils {
         }
 
         bool isBissexto(int ano) {
-        return ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0));
+            return ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0));
         }
 
         bool isValidDate(string date) {
