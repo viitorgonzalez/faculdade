455b292c92622c8aaed16d1cf004ac14420e0e32
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-09 16:48:59 -0300

    refactor(TrabalhoFinal): programacao-2\Trabalho-final\Falta.cpp

--- programacao-2/Trabalho-final/Falta.cpp
+++ programacao-2/Trabalho-final/Falta.cpp
@@ -1,48 +1,46 @@
-#pragma once
-
 #include "Aluno.cpp"
 #include "Disciplina.cpp"
 
 using namespace std;
 
 class Falta {
-private:
-    Aluno aluno;
-    Disciplina disciplina;
-    string *data;
+    private:
+        Aluno aluno;
+        Disciplina disciplina;
+        string *data;
 
-public:
-    Falta() {
-        data = new string[10];
-    }
+    public:
+        Falta() {
+            data = new string[10];
+        }
 
-    Falta(Aluno aluno, Disciplina disciplina) {
-        this->disciplina = disciplina;
-        this->aluno = aluno;
-        data = new string[10];
-    }
+        Falta(Aluno aluno, Disciplina disciplina) {
+            this->disciplina = disciplina;
+            this->aluno = aluno;
+            data = new string[10];
+        }
 
-    Aluno getAluno() {
-        return aluno;
-    }
+        Aluno getAluno() {
+            return aluno;
+        }
 
-    void setAluno(Aluno aluno) {
-        this->aluno = aluno;
-    }
+        void setAluno(Aluno aluno) {
+            this->aluno = aluno;
+        }
 
-    Disciplina getDisciplina() {
-        return disciplina;
-    }
+        Disciplina getDisciplina() {
+            return disciplina;
+        }
 
-    void setDisciplina(Disciplina disciplina) {
-        this->disciplina = disciplina;
-    }
-    
-    string *getData(){
-    	return data;
-	}
-    
-	void setData(string *data){
-		this->data = data;
-	}
+        void setDisciplina(Disciplina disciplina) {
+            this->disciplina = disciplina;
+        }
+        
+        string *getData(){
+            return data;
+        }
+        
+        void setData(string *data){
+            this->data = data;
+        }
 };
