f17ce5817f6dc06bee991477e9bd97330dd64135
viitorgonzalez <pastor123.vs@gmail.com>
2024-06-09 16:48:17 -0300

    refactor(TrabalhoFinal): programacao-2\Trabalho-final\CadastroFalta.cpp

--- programacao-2/Trabalho-final/CadastroFalta.cpp
+++ programacao-2/Trabalho-final/CadastroFalta.cpp
@@ -1,21 +1,19 @@
-#pragma once
-
 #include "Falta.cpp"
 
 class CadastroFalta {
-private:
-    Falta *turmaFaltas;
+    private:
+        Falta *turmaFaltas;
 
-public:
-    CadastroFalta() {
-        turmaFaltas = new Falta[10];
-    }
+    public:
+        CadastroFalta() {
+            turmaFaltas = new Falta[10];
+        }
 
-    Falta *getTurmaFaltas() {
-        return turmaFaltas;
-    }
+        Falta *getTurmaFaltas() {
+            return turmaFaltas;
+        }
 
-    void setTurmaFaltas(Falta *turmaFaltas) {
-        this->turmaFaltas = turmaFaltas;
-    }
+        void setTurmaFaltas(Falta *turmaFaltas) {
+            this->turmaFaltas = turmaFaltas;
+        }
 };
